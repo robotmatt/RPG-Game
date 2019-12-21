c61da69102cd44fe2b45653b3ad9b28a91f19761
Matt Martin <matt@incgn.to>
2019-12-21 10:43:16 -0700

    add refresh button to page

---
+++ assets/js/game.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    const characters = [
+        {
+            name: "Obi Wan",
+            image: "",
+            power: 120
+        },
+        {
+            name: "Luke Skywalker",
+            image: "",
+            power: 100
+        },
+        {
+            name: "Darth Sidious",
+            image: "",
+            power: 150
+        },
+        {
+            name: "Darth Maul",
+            image: "",
+            power: 180
+        },
+    ];
+
+    let myCharacterIndex = -1;
+    let myDefenderIndex = -1;
+
+    function startGame(){
+        $("#characters").
+    }
+});
